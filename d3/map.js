5b4c764cd6bab002894fc2a22f0f894c31e161f8
jimmy <jinyicong@jimmys-MacBook-Pro.local>
2018-08-23 00:39:30 +1000

    d3 exercise

---
+++ d3/map.js
@@ -0,0 +1,7 @@
+d3.select("#mapSection")
+    .selectAll("div")
+    .data(data)
+    .enter()
+    .append("div")
+    .style("width", function(d) { return d + "px"; })
+    .text(function(d) { return d; });
