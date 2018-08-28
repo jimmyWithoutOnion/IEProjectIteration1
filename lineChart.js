5bcf163f72b1ea5cb9effaca7d405ff619e11a2e
jmy726 <442332016@qq.com>
2018-08-28 23:17:50 +1000

    updata line chart again

--- lineChart.js
+++ lineChart.js
@@ -23,7 +23,7 @@ var DATA = {
         "bricks": [994506.75, 138276.75, 653175.0, 447867.75, 554217.75, 9411.0, 905342.25, 967231.5, 238196.25, 266019.0, 888836.25, 269091.0, 1107330.75, 598761.0, 628780.5, 840102.75],
         "tiles": [3182421.6, 442485.6, 2090160.0, 1433176.8, 1773496.8, 30115.199999999997, 2897095.1999999997, 3095140.8, 762228.0, 851260.7999999999, 2844276.0, 861091.2, 3543458.4, 1916035.2, 2012097.5999999999, 2688328.8]
     }
-}
+};
 
 var YEAR = DATA.data.year;
 
@@ -32,34 +32,34 @@ var config = {
     data: {
         labels: DATA.data.year,
         datasets: [{
-            label: 'timber',
+            label: 'Timber',
             backgroundColor: 'rgb(255, 99, 132)',
             borderColor: 'rgb(255, 99, 132)',
             data: DATA.data.timber,
             fill: false,
         }, {
-            label: 'plasterboard',
+            label: 'Plasterboard',
             fill: false,
             backgroundColor: 'rgb(98, 223, 255)',
             borderColor: 'rgb(98, 223, 255)',
             data: DATA.data.plasterboard,
         }, {
-            label: 'concrete',
+            label: 'Concrete',
             fill: false,
             backgroundColor: 'rgb(205, 97, 255)',
             borderColor: 'rgb(205, 97, 255)',
             data: DATA.data.concrete,
         }, {
-            label: 'bricks',
+            label: 'Bricks',
             fill: false,
             backgroundColor: 'rgb(63, 132, 63)',
             borderColor: 'rgb(63, 132, 63)',
             data: DATA.data.bricks,
         }, {
-            label: 'tiles',
+            label: 'Tiles',
             fill: false,
-            backgroundColor: 'rgb(229, 139, 103)',
-            borderColor: 'rgb(229, 139, 103)',
+            backgroundColor: 'rgb(242, 242, 26)',
+            borderColor: 'rgb(242, 242, 26)',
             data: DATA.data.tiles,
         }],
         options: {
@@ -93,7 +93,7 @@ var config = {
                     scaleLineColor: 'rgb(179, 242, 237)',
                     scaleLabel: {
                         display: true,
-                        labelString: 'Value'
+                        labelString: 'Tonnes of Waste'
                     }
                 }]
             }
@@ -101,26 +101,33 @@ var config = {
     }
 };
 
+function getSelectSuburb()
+{
+    var selectedValue = document.getElementById("SuburbSelect").value;
+    DATA.data.area = selectedValue;
+    console.log(selectedValue);
+}
+
 window.onload = function () {
     var ctx = document.getElementById('canvas').getContext('2d');
     window.myLine = new Chart(ctx, config);
 };
 
-document.getElementById('addData').addEventListener('click', function (datasets) {
-    if (config.data.datasets.length > 0) {
-        var year = YEAR[config.data.labels.length % YEAR.length];
-        config.data.labels.push(year);
-        config.data.datasets.forEach(function (dataset) {
-            dataset.data.push(datasets);
-        });
-        window.myLine.update();
-    }
-});
-
-document.getElementById('removeData').addEventListener('click', function () {
-    config.data.labels.splice(-1, 1); // remove the label first
-    config.data.datasets.forEach(function (dataset) {
-        dataset.data.pop();
-    });
-    window.myLine.update();
-});
+// document.getElementById('addData').addEventListener('click', function (datasets) {
+//     if (config.data.datasets.length > 0) {
+//         var year = YEAR[config.data.labels.length % YEAR.length];
+//         config.data.labels.push(year);
+//         config.data.datasets.forEach(function (dataset) {
+//             dataset.data.push(datasets);
+//         });
+//         window.myLine.update();
+//     }
+// });
+//
+// document.getElementById('removeData').addEventListener('click', function () {
+//     config.data.labels.splice(-1, 1); // remove the label first
+//     config.data.datasets.forEach(function (dataset) {
+//         dataset.data.pop();
+//     });
+//     window.myLine.update();
+// });
